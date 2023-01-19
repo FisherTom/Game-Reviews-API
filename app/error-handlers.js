d7e2e3019279ede0bd956729a85f74c3e7a44e34
fishertom <tfisher92@gmail.com>
2023-01-19 17:21:11 +0000

    remove comments

--- app/error-handlers.js
+++ app/error-handlers.js
@@ -1,18 +1,6 @@
-// app.all("/*", (request, response) => {
-//     response.status(404).send({ msg: "Not Found" });
-//   }); // if no endpoints are matched
-
 function endPointNotFound(request, response) {
   response.status(404).send({ msg: "Not Found" });
 }
-
-//   app.use((err, request, response, next) => {
-//     if (err.status) {
-//       response.status(err.status).send({ msg: err.msg });
-//     } else {
-//       next(err);
-//     }
-//   });
 
 function customError(err, request, response, next) {
   if (err.status) {
@@ -21,17 +9,6 @@ function customError(err, request, response, next) {
     next(err);
   }
 }
-
-//   app.use((err, request, response, next) => {
-//     if (err.code === "23503") {
-//       response.status(404).send({ msg: "Not found" });
-//     }
-//     if (err.code === "22P02" || "23502") {
-//       response.status(400).send({ msg: "Bad request" });
-//     } else {
-//       next(err);
-//     }
-//   });
 
 function postgresError(err, request, response, next) {
   if (err.code === "23503") {
@@ -43,10 +20,6 @@ function postgresError(err, request, response, next) {
     next(err);
   }
 }
-
-//   app.use((err, request, response, next) => {
-//     response.status(500).send({ msg: "internal sever error" });
-//   });
 
 function internalServerError(err, request, response, next) {
   response.status(500).send({ msg: "internal sever error" });
